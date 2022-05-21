38c3a9cae63fa45a6360cf9d11856f206f3c71e4
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-21 19:02:34 +0500

    sanity backend on git

---
+++ schemas/works.js
@@ -0,0 +1,54 @@
+export default {
+  name: 'works',
+  title: 'Works',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'featured',
+      title: 'Featured',
+      type: 'boolean',
+    },
+    {
+      name: 'projectLink',
+      title: 'Project Link',
+      type: 'string',
+    },
+    {
+      name: 'codeLink',
+      title: 'Code Link',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImageUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
+        },
+      ],
+    },
+  ],
+}
